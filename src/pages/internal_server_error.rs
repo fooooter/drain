a298f24ee8754156d025db761cc35221966f55bb
footer <0xb00b1e5@proton.me>
2025-01-25 10:05:30 +0100

    Add the version information at the bottom in internal_server_error.rs.

--- src/pages/internal_server_error.rs
+++ src/pages/internal_server_error.rs
@@ -7,7 +7,21 @@ pub async fn internal_server_error<T>(mut stream: &mut T) -> Result<(), Box<dyn 
 where
     T: AsyncRead + AsyncWrite + Unpin
 {
-    let content: Vec<u8> = Vec::from(include_str!("internal_server_error.html"));
+    let content: Vec<u8> = Vec::from(format!(r#"
+    <!DOCTYPE html>
+    <html lang="en">
+        <head>
+            <meta charset="utf-8">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <title>500</title>
+        </head>
+        <body>
+            <h2>500 Internal Server Error</h2>
+        </body>
+        <hr>
+        <small>Drain {}</small>
+    </html>
+    "#, env!("CARGO_PKG_VERSION")));
 
     let response_headers = HashMap::from([(String::from("Content-Type"), String::from("text/html; charset=utf-8"))]);
     send_response(&mut stream, None, 500, Some(response_headers), Some(content), None).await
